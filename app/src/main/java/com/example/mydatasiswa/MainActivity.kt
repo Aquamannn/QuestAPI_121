7521df8fb26d5f2859f6b122bbf8de08437eaa2d
Lenovo <9.3razzinayyuri@gmail.com>
2025-09-02 19:19:18 +0700

    21

--- app/src/main/java/com/example/mydatasiswa/MainActivity.kt
+++ app/src/main/java/com/example/mydatasiswa/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
             MyDataSiswaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     DataSiswaApp(
-                        modifier = Modifier.padding(innerPadding)19
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
